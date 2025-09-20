dd1e933e42e64f410138bfc1d46283b3d44f9ad6
sandipmori-max <[200~sandipmori-max>
2025-09-20 18:36:03 +0530

    'WORKIGN_CODE___1'

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,7 +4,7 @@ import { DrawerItemConfig } from '../components/types';
 export const ERP_DRAWER_LIST: DrawerItemConfig[] = [
   { label: 'Home', route: 'Home', icon: 'home' },
   { label: 'Attendance', route: 'Attendance', icon: 'calendar-month' },
-  { label: 'Display', route: 'Display', icon: 'computer' },
+  // { label: 'Display', route: 'Display', icon: 'computer' },
   { label: 'Alert', route: 'Alert', icon: 'notifications-active' },
   { label: 'Privacy Policy', route: 'Privacy Policy', icon: 'policy' },
 ];
